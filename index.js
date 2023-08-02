13283b140e8ea855ee8a68ff692183cc533583aa
mollytodd <131923397+mollytodd@users.noreply.github.com>
2023-08-02 12:13:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-function addingEventListener() {
-    const input = document.getElementById('button');
-  
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-  
-    input.addEventListener('click', clickAlert);
-  }
+const input = document.getElementById('button');
+
+function clickAlert (){
+    alert('HI THERE MOLLY')
+}
+
+const addingEventListener= ()=>{
+    input.addEventListener('click', clickAlert)}
